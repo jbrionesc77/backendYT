b1df52ea18831516d09cd363d0cc7700bbe77a68
TODO1\jbriones <jbriones@todo1.com>
2018-12-06 09:44:34 -0500

    TercerCommit

---
+++ src/main/java/com/youtube/demo/service/userServiceImpl.java
@@ -0,0 +1,18 @@
+package com.youtube.demo.service;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.youtube.demo.dao.UserRepository;
+import com.youtube.demo.model.User;
+
+@Service
+public class userServiceImpl implements UserService {
+	@Autowired
+	protected UserRepository userRepository;
+	
+	@Override
+	public User save(User user) {
+		return this.userRepository.save(user);
+	}
+
+}
